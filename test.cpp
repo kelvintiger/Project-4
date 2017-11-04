d928e7f3d3c9be95740aac38ec0250ec8a2722a3
kelvintiger <33381772+kelvintiger@users.noreply.github.com>
2017-11-04 17:42:56 -0400

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,127 @@
+/**
+ * test.cpp
+ *
+ * EECS 183, Fall 2017
+ * Project 4: CoolPics
+ *
+ * Kelvin Chang
+ * kelvinct
+ *
+ * Testing all the implementation in Project 4
+ */
+
+#include <iostream>
+#include <fstream>
+#include "Point.h"
+#include "Color.h"
+#include "Graphics.h"
+#include "Line.h"
+#include "Triangle.h"
+#include "Circle.h"
+#include "Rectangle.h"
+#include "pics.cpp"
+#include "bmp.h"
+
+using namespace std;
+
+
+//Test Functions
+void test_point();
+void test_Color();
+void test_Graphics();
+
+//Main testing
+int main() {
+    test_point();
+    
+    test_Color();
+    
+    
+    
+    return 0;
+}
+
+//Testing point.cpp
+void test_point() {
+    
+    Point pt1;
+    pt1.setX(15);
+    cout << "pt1 is: " << pt1 << endl;
+    ifstream input_file;
+    input_file.open("data1.txt");
+    pt1.read(input_file);
+    cout << "pt1 is: " << pt1 << endl;
+    
+    Point pt2;
+    cout << "pt2 is: " << pt2 << endl;
+    
+    Point pt3(-50, 1000);
+    cout << "pt3 is: " << pt3 << endl;
+    
+    Point pt4;
+    pt4.setY(100);
+    cout << "pt4 is: " << pt4 << endl;
+    
+    Point pt5;
+    cout << "x of pt5 is: " << pt5.getX() << endl;
+    
+    Point pt6;
+    pt6.setX(-1000);
+    pt6.setX(1000);
+    pt6.setX(0);
+    pt6.setX(100);
+    pt6.setX(99);
+    pt6.setX(69);
+    
+    cout<< "pt6 is: " << pt6 << endl;
+    
+    
+    return;
+}
+
+//Testing Color.cpp
+void test_Color(){
+    
+    Color c1;
+    cout << "c1 is:" << endl;
+    
+    Color c2(50, 50, 50);
+    cout << "c2 is: " << endl;
+    
+    Color c3(-1, 1000, 0);
+    cout << "c3 is: " << endl;
+    
+    Color c4;
+    ifstream input_file;
+    input_file.open("data1.txt");
+    c4.read(input_file);
+    cout << "pt1 is: " << c4 << endl;
+    
+    Color c5;
+    c5.setRed(-1000);
+    cout << "c5 is: " << c5;
+    c5.setRed(0);
+    cout << "c5 is: " << c5;
+    c5.setRed(1000);
+    cout << "c5 is: " << c5;
+    
+    c5.setBlue(-1000);
+    cout << "c5 is: " << c5;
+    c5.setBlue(0);
+    cout << "c5 is: " << c5;
+    c5.setBlue(1000);
+    cout << "c5 is: " << c5;
+    
+    c5.setGreen(-1000);
+    cout << "c5 is: " << c5;
+    c5.setGreen(0);
+    cout << "c5 is: " << c5;
+    c5.setGreen(1000);
+    cout << "c5 is: " << c5;
+    
+    Color c6;
+    cout << "(" << c6.getRed() << "," << c6.getBlue() << "," << c6.getGreen() << ")" << endl;
+    
+}
+
+
